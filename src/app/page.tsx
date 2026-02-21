82a69a93d4e44a811a0b48cdcca1aeb47db47737
sidekick012 <9135975+sidekick012@users.noreply.github.com>
2026-02-21 20:13:25 +0000

    fix: getThemeToggler() error - removed the import from /src/app/page.tsx - redeploying to enable Google OAth.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { sql } from "drizzle-orm";
 import { auth } from "@/server/auth";
 import { db } from "@/server/db";
 import { users } from "@/server/db/schema";
-import { getThemeToggler } from "@/lib/theme/get-theme-button";
+import { ThemeToggler } from "@/lib/theme/get-theme-button";
 import { signOutAction, googleSignInAction } from "./actions";
 
 export const runtime = "edge";
